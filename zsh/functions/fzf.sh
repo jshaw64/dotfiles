2a42a19c595d61d5ed69292d3f05ac892c94bf59
jshaw <jshaw64@gmail.com>
2019-09-08 00:15:09 -0400

    Add fzfag to fzf functions

--- zsh/functions/fzf.sh
+++ zsh/functions/fzf.sh
@@ -5,3 +5,15 @@ fzfe() {
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
+
+fzfag() {
+  local file
+  local line
+
+  read -r file line <<<"$(ag --nobreak --noheading $@ | fzf -0 -1 | awk -F: '{print $1, $2}')"
+
+  if [[ -n $file ]]
+  then
+     "$EDITOR" $file +$line
+  fi
+}
