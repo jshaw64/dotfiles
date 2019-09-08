7cffef55598829cf3951a6b545e5f937dbf2ab93
jshaw <jshaw64@gmail.com>
2019-09-08 00:20:45 -0400

    Add fbr to fzf functions

--- zsh/functions/fzf.sh
+++ zsh/functions/fzf.sh
@@ -24,3 +24,12 @@ fcdf() {
    local dir
    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
 }
+
+# checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fbr() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
