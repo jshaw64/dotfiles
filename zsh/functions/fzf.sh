a8a9329cc86fe8302c87566dd40a64ef56824b6b
jshaw <jshaw64@gmail.com>
2019-09-08 00:07:54 -0400

    Create fzf functions with fzfe

---
+++ zsh/functions/fzf.sh
@@ -0,0 +1,7 @@
+# From https://github.com/junegunn/fzf/wiki/examples
+
+fzfe() {
+  local files
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
